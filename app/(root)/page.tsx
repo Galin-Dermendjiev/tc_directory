d0d8d470b617a798a7187cde8cd6cfad493dad96
Galin <sao4449@abv.bg>
2025-11-12 18:02:48 +0200

    Add hero section and fix font configuration issues

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      Home
+      <section className="blue_container">
+        <h1 className="heading">Share your startup, <br/> Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Your Ideas Now.</p>
+      </section>
+
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type {Config} from "tailwindcss";
 
 const config: Config = {
-    darkMode: "class",
+    darkMode: ["class"],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,10 +15,10 @@ const config: Config = {
             },
             colors: {
                 primary: {
-                    "100": "#FFE8F0",
-                    DEFAULT: "#EE2B69",
+                    "100": "#EAF4FF",  // pale blue background tint
+                    DEFAULT: "#2B7FFF", // vibrant blue
                 },
-                secondary: "#FBE843",
+                secondary: "#FF6F61", // coral accent
                 black: {
                     "100": "#333333",
                     "200": "#141413",
