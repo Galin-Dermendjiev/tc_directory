cad4277df310d5ac1b42e1a0d56fe968fa64feb1
Galin <sao4449@abv.bg>
2025-11-16 16:13:10 +0200

    Add create page

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+import StartupForm from "@/components/StartupForm";
+import { redirect } from "next/navigation";
+
+export default async function Page(){
+    const session = await auth()
+
+    if(!session) redirect('/')
+    return <>
+        <section className="blue_container !min-h-[230px]">
+            <h1 className="heading">Submit Your Startup</h1>
+        </section>
+
+        <StartupForm />
+    </>
+}
