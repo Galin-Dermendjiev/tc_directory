180a814e384e157b8c095c3221bc6175c18dac40
Galin <sao4449@abv.bg>
2025-11-20 21:41:44 +0200

    Make navbar responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar } from "./ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 
 export default async function Navbar(){
@@ -15,18 +18,24 @@ export default async function Navbar(){
                 {session && session?.user ? (
                     <>
                         <Link href="/startup/create">
-                            <span>Create</span>
+                            <span className="max-sm:hidden">Create</span>
+                            <BadgePlus className="size-6 sm:hidden"/>
                         </Link>
                         <form action={async () => {
                             'use server'
                             await signOut({redirectTo: "/"})
                         }}>
-                            <button type="submit">Logout</button>
+                            <button type="submit">
+                                <span className="max-sm:hidden">Logout</span>
+                                <LogOut className="size-6 sm:hidden text-black-500"/>
+                            </button>
                         </form>
                         
 
                         <Link href={`/user/${session?.id}`}>
-                            <span>{session?.user?.name}</span>
+                            <Avatar className="size-10">
+                                <AvatarImage src={session?.user?.image || ''} alt={session?.user?.name || ''}/> 
+                            </Avatar>
                         </Link>
                     </>
                 ) : (
