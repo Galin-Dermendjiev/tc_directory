fe5111a92c1b70d718f44a81b0cd9c4a67c492cd
Galin <sao4449@abv.bg>
2025-11-21 16:04:44 +0200

    Remove console log

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,9 +11,6 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
   const query = (await searchParams).query
   const params = {search: query || null}
 
-  const session = await auth()
-console.log(session?.id)
-
   const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
